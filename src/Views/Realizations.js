96083dc3ff928d54a7e95569552e70c9dc94cda5
Rafał <rafal@MacBook-Pro-mac.local>
2020-09-15 20:30:16 +0200

    realizations lightbox

--- src/Views/Realizations.js
+++ src/Views/Realizations.js
@@ -1,19 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import { projects } from "../api"
+import Lightbox from "../components/Lightbox"
 
-const Realizations = () => (
-  <div className="container section">
-    <h2 className="headline headline--center">
-      Realizacje
-    </h2>
-    <div className="gallery">
-      {projects.map(project => (
-        <div className="gallery__item">
-          <img src={project.url} alt={project.title} />
+const Realizations = () => {
+  const [modal, setModal] = useState(false)
+  const [indexImage, setIndexImage] = useState(false)
+
+  const openModal = (index) => {
+    setModal(true)
+    setIndexImage(index)
+  }
+
+  const closeModal = () => {
+    setModal(false)
+  }
+
+  return (
+    <>
+      {modal && <Lightbox index={indexImage} closeModal={closeModal} tab={projects} />}
+      <div className="container section">
+        <h2 className="headline headline--center">
+          Realizacje
+        </h2>
+        <div className="gallery">
+          {projects.map((project) => (
+            <div key={project.id} className="gallery__item" onClick={() => openModal(projects.indexOf(project)) }>
+              <img src={project.url} alt={project.title} />
+              <div className="gallery__cover">{project.title}</div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </div>
-)
+      </div>
+    </>
+  )
+}
 
 export default Realizations
